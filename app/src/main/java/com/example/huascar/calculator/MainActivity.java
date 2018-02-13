acf210ed437a41f96916e63e6258fec655948b26
Huascar <huascarmc@hotmail.com>
2018-02-13 18:12:08 +0000

    added dimen values build succesful for layout

--- app/src/main/java/com/example/huascar/calculator/MainActivity.java
+++ app/src/main/java/com/example/huascar/calculator/MainActivity.java
@@ -3,16 +3,28 @@ package com.example.huascar.calculator;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.huascar.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBindibng binding;
+    private ActivityMainBinding binding;
     private double valueOne = Double.NaN;
     private double valueTwo;
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
+
+    private char CURRENT_ACTION;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         }
+
+        private void getValue(View view) {
+
     }
 }

9ed4d880c1d94de5156eecf2057af330594a2a99
Huascar <huascarmc@hotmail.com>
2018-02-13 17:40:59 +0000

    calculator layout set

--- app/src/main/java/com/example/huascar/calculator/MainActivity.java
+++ app/src/main/java/com/example/huascar/calculator/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.huascar.calculator;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBindibng binding;
+    private double valueOne = Double.NaN;
+    private double valueTwo;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        }
     }
 }

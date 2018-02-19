3e6d7899cf48d4b17cddfe91294f31dd3ca988e1
Huascar <huascarmc@hotmail.com>
2018-02-19 16:02:47 +0000

    add/subtract tests added not passing

---
+++ app/src/test/java/com/example/huascar/calculator/CalculatorTest.java
@@ -0,0 +1,23 @@
+package com.example.huascar.calculator;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by huascar on 19/02/2018.
+ */
+
+public class CalculatorTest {
+
+    @Test
+    public void additionTest() throws Exception {
+//        assertEquals();
+    }
+
+    @Test
+    public void subtractionTest() throws Exception {
+//        assertEquals();
+    }
+}
